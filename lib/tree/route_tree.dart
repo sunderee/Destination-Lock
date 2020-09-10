39e1502e033bc926cfdfc35bc0b2c4386b717e49
Peter Aleksander Bizjak <peter.aleksander.bizjak@ipmgroup.si>
2020-09-10 18:25:38 +0200

    Build the destination tree

---
+++ lib/tree/route_tree.dart
@@ -0,0 +1,23 @@
+import 'package:destination_lock/tree/models/node.dart';
+import 'package:destination_lock/tree/models/regular_route.dart';
+import 'package:destination_lock/tree/models/root_route.dart';
+import 'package:destination_lock/tree/tree_builder.dart';
+import 'package:flutter/material.dart';
+
+class RouteTree {
+  final RootRoute rootRoute;
+  final Iterable<RegularRoute> regularRoutes;
+
+  RouteTree({this.rootRoute, this.regularRoutes});
+
+  Map<String, WidgetBuilder> convertToTreeStructure() {
+    Node<Widget, String> root = rootRoute.toNode();
+    regularRoutes.forEach((route) {
+      root.insertChild(route.toNode());
+    });
+
+    final DestinationTree destinationTree = DestinationTree();
+    destinationTree.buildRouteMap(root);
+    return destinationTree.getRouteMap();
+  }
+}
