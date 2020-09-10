71660b46b3851696f96c4ac38fda54f40111c003
Peter Aleksander Bizjak <peter.aleksander.bizjak@ipmgroup.si>
2020-09-10 10:23:28 +0200

    Begin working on the destinations tree and methods associated with it

--- lib/tree/destination_tree.dart
+++ lib/tree/destination_tree.dart
@@ -1,18 +1,37 @@
+import 'package:destination_lock/structs/pair.struct.dart';
 import 'package:destination_lock/tree/node.dart';
 import 'package:flutter/material.dart';
 
-Map<String, dynamic> buildRouteMap<T, S>(Node<T, S> root) {
+Map<String, dynamic> buildRouteMap(Node<String, Widget> root) {
   Map<String, WidgetBuilder> routeMap = Map();
+
+  if (root.hasChildren()) {
+    List<Pair<String, WidgetBuilder>> routesList = returnChildRoutes(root);
+    routesList.forEach((route) {
+      routeMap.putIfAbsent(route.first, () => route.second);
+    });
+    return routeMap;
+  } else {
+    return null;
+  }
 }
 
-List<Map<String, dynamic>> generateChildRoutes<T, S>(Node<T, S> currentNode) {
-  if (currentNode.children.isEmpty) {
-    return [];
-  } else {
-    List<Map<dynamic, dynamic>> tmpList = [];
-    currentNode.children.forEach((element) {
-      tmpList.add({element.reference: element.data});
+List<Pair<String, WidgetBuilder>> returnChildRoutes(Node<String, Widget> node) {
+  if (node.hasChildren()) {
+    List<Pair<String, WidgetBuilder>> tmpList = [];
+    node.children.forEach((child) {
+      tmpList.add(
+        Pair<String, WidgetBuilder>(
+            first: child.reference, second: (BuildContext cntx) => child.data),
+      );
+      if (child.hasChildren()) {
+        child.children.forEach((innerChild) {
+          returnChildRoutes(innerChild);
+        });
+      }
     });
     return tmpList;
+  } else {
+    return [];
   }
 }
