176584b8f6694bc1fdc05c86087a3f6456f5c588
Peter Aleksander Bizjak <peter.aleksander.bizjak@ipmgroup.si>
2020-09-11 11:43:25 +0200

    Update documentation

--- lib/destination_lock.dart
+++ lib/destination_lock.dart
@@ -15,10 +15,13 @@ class DestinationLock extends MaterialApp {
   /// Collection of regular routes
   final List<RegularRoute> regularRoutes;
 
+  /// Collection of blocked routes
   final List<String> blockedRoutes;
 
   /// Create the [DestinationLock] via the [RootRoute] and collection of
-  /// [RegularRoute]s
+  /// [RegularRoute] objects. Both [rootRoute] and [regularRoutes] paramters are
+  /// required, but you also have the [blockedRoutes] one which defines routes
+  /// that are supposed to be blocked
   DestinationLock(
       {@required this.rootRoute,
       @required this.regularRoutes,
@@ -39,6 +42,10 @@ class DestinationLock extends MaterialApp {
     return buildRouteMap(root);
   }
 
+  /// Recomputes tree structure based on the blocked routes predicament: you're
+  /// only supposed to remove routes whose path contains one of the blocked
+  /// path mentions, but that means that the structure of paths (this is, their
+  /// parent-child relative paths) need to follow a strict naming convention
   void recomputeTree(List<String> blockedRoutes) {
     regularRoutes.removeWhere((route) {
       for (int i = 0; i < blockedRoutes.length; i++) {
